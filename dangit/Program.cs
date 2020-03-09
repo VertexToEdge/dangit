89c611303f90203dabfbc536f050208ab63a0b44
VertexToEdge <vertextoedge@gmail.com>
2020-03-09 20:00:32 +0900

    first commit

---
+++ dangit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace dangit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+     
+            String cDir = Directory.GetCurrentDirectory();
+            Console.WriteLine(cDir);
+        }
+    }
+}
